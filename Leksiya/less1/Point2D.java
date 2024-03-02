9b9572685227e770f76d1db6197427bd89b7356d
Sirojiddin <sirojboy@inbox.ru>
2024-03-02 12:23:57 +0500

    add files

---
+++ Leksiya/less1/Point2D.java
@@ -0,0 +1,54 @@
+package Leksiya.less1;
+
+/**
+ * Bu 2D nuqta
+ */
+public class Point2D {
+    private int x, y;
+
+    /**
+     * Bu konstruktor ...
+     * @param valueX bu X koordinatasi
+     * @param valueY bu Y koordinatasi
+     */
+    public Point2D(int valueX, int valueY) {
+        x = valueX;
+        y = valueY;
+    }
+
+    public Point2D(int value) {
+        this(value, value);
+    }
+
+    public Point2D() {
+        this(0);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int value) {
+        this.x = value;
+    }
+
+    public void setY(int value) {
+        this.y = value;
+    }
+
+    private String getInfo() {
+        return String.format("x: %d, y: %d", x, y);
+    }
+    @Override
+    public String toString() {
+        return getInfo();
+    }
+
+    public static double distance(Point2D a, Point2D b) {
+        return Math.sqrt(Math.pow(a.x - b.x, 2)+ Math.pow(a.y - b.y, 2));
+    }
+}
